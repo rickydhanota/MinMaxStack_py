36e337d7d28f4667f26ac36fa3e194a123f764b7
Ricky <rickdhanota@gmail.com>
2020-11-04 22:02:43 -0800

    initial commit

---
+++ minMaxStackConstruction.py
@@ -0,0 +1,23 @@
+# Min max stack construction
+#Write a MinMaxStack class for a min max stack. The class should support:
+#   -Pushing and popping values on and off the stack
+#   -Peeking at the value at the top of the stack
+#   -Getting both the minimum and the maximum values in the stack at any given point in time 
+
+#All class methods, when considered independently should run in constant time and with constant space.
+
+class MinMaxStack:
+    def peek(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def push(self, number):
+        pass
+
+    def getMin(self):
+        pass
+
+    def getMax(self):
+        pass
